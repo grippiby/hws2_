34ecea8c18b5b9c9ccad76cc2c920c16c271c7df
Svyatoslav Nilikovsky <grippiby@gmail.com>
2023-06-30 01:16:44 +0300

    added 10HW

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,9 @@
 import s from './Loader.module.css'
+import { ReactComponent as Loading } from '../../svg/Spinner-1s-200px.svg'
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => (
+	<div className={s.loader}>
+		<Loading />
+	</div>
+)
+/*export const Loader = () => <div className={s.loader}/>*/
--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,22 +1,35 @@
-const initState = {
-    isLoading: false,
+export type MyInitStateType = {
+	isLoading: boolean
+}
+//может выше и не надо
+
+const initState: MyInitStateType = {
+	isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // пишет студент  // need to fix
-
-        default:
-            return state
-    }
+export const loadingReducer = (
+	state = initState,
+	action: LoadingActionType
+): MyInitStateType => {
+	// fix any
+	switch (action.type) {
+		case 'CHANGE_LOADING': {
+			return {
+				isLoading: action.isLoading,
+			}
+		}
+		// пишет студент  // need to fix
+		default:
+			return state
+	}
 }
 
 type LoadingActionType = {
-    type: 'CHANGE_LOADING'
-    isLoading: boolean
+	type: 'CHANGE_LOADING'
+	isLoading: boolean
 }
 
 export const loadingAC = (isLoading: boolean): LoadingActionType => ({
-    type: 'CHANGE_LOADING',
-    isLoading,
+	type: 'CHANGE_LOADING',
+	isLoading,
 })
