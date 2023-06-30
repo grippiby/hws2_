9897f0086adffb9454c371ff40a3a0e44237fc39
Svyatoslav Nilikovsky <grippiby@gmail.com>
2023-07-01 00:29:13 +0300

    added 11hw

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -5,47 +5,70 @@ import { restoreState } from '../hw06/localStorage/localStorage'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 
 /*
-* 1 - передать значения в оба слайдера
-* 2 - дописать типы и логику функции change
-* 3 - сделать стили в соответствии с дизайном
-* */
+ * 1 - передать значения в оба слайдера
+ * 2 - дописать типы и логику функции change
+ * 3 - сделать стили в соответствии с дизайном
+ * */
 
 function HW11() {
-    // for autotests // не менять // можно подсунуть в локалСторэдж нужные числа, чтоб увидеть как они отображаются
-    const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
-    const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
+	// for autotests // не менять // можно подсунуть в локалСторэдж нужные числа, чтоб увидеть как они отображаются
+	const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
+	const [value2, setValue2] = useState(
+		restoreState<number>('hw11-value2', 100)
+	)
 
-    const change = (event: any, value: any) => {
-        // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
-    }
+	const change = (event: Event, value: number | number[]) => {
+		if (Array.isArray(value)) {
+			const [value1, value2] = value
 
-    return (
-        <div id={'hw11'}>
-            <div className={s2.hwTitle}>Homework #11</div>
+			setValue1(value1)
+			setValue2(value2)
+		} else {
+			setValue1(value)
+		}
 
-            <div className={s2.hw}>
-                <div className={s.container}>
-                    <div className={s.wrapper}>
-                        <span id={'hw11-value'} className={s.number}>{value1}</span>
-                        <SuperRange
-                            id={'hw11-single-slider'}
-                            // сделать так чтоб value1 изменялось // пишет студент
+		// пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
+	}
 
-                        />
-                    </div>
-                    <div className={s.wrapper}>
-                        <span id={'hw11-value-1'} className={s.number}>{value1}</span>
-                        <SuperRange
-                            id={'hw11-double-slider'}
-                            // сделать так чтоб value1/2 изменялось // пишет студент
+	return (
+		<div id={'hw11'}>
+			<div className={s2.hwTitle}>Homework #11</div>
 
-                        />
-                        <span id={'hw11-value-2'} className={s.number}>{value2}</span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
+			<div className={s2.hw}>
+				<div className={s.container}>
+					<div className={s.wrapper}>
+						<span id={'hw11-value'} className={s.number}>
+							{value1}
+						</span>
+						<SuperRange
+							id={'hw11-single-slider'}
+							onChange={change}
+							value={value1}
+							step={1}
+							valueLabelDisplay="auto"
+							// сделать так чтоб value1 изменялось // пишет студент
+						/>
+					</div>
+					<div className={s.wrapper}>
+						<span id={'hw11-value-1'} className={s.number}>
+							{value1}
+						</span>
+						<SuperRange
+							id={'hw11-double-slider'}
+							value={[value1, value2]}
+							onChange={change}
+							disableSwap
+
+							// сделать так чтоб value1/2 изменялось // пишет студент
+						/>
+						<span id={'hw11-value-2'} className={s.number}>
+							{value2}
+						</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default HW11
--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
-import {Slider, SliderProps} from '@mui/material'
+import { Slider, SliderProps } from '@mui/material'
 
 const SuperRange: React.FC<SliderProps> = (props) => {
-    return (
-        <Slider
-            sx={{ // стили для слайдера // пишет студент
-                
-            }}
-            {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
-        />
-    )
+	console.log(props.value)
+	return (
+		<Slider
+			sx={{
+				// стили для слайдера // пишет студент
+				width: '150px',
+				color: '#3a8589',
+			}}
+			{...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
+		/>
+	)
 }
 
 export default SuperRange
