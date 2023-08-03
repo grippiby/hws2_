a44c3fb668ab2b1af2669091d23127c9e5f3f5bd
Svyatoslav Nilikovsky <grippiby@gmail.com>
2023-08-03 10:32:10 +0300

    added 12 HW

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,31 @@
+type SwitchThemeType = {
+	type: 'SET_THEME_ID'
+	id: number
+}
+export type StateType<T> = { themeId: T }
+
 const initState = {
-    themeId: 1,
+	themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // дописать
-
-        default:
-            return state
-    }
+export const themeReducer = (
+	state: StateType<number> = initState,
+	action: SwitchThemeType
+): StateType<number> => {
+	// fix any
+	switch (action.type) {
+		// дописать
+		case 'SET_THEME_ID':
+			return {
+				...state,
+				themeId: action.id,
+			}
+		default:
+			return state
+	}
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+export const changeThemeId = (id: number): SwitchThemeType => ({
+	type: 'SET_THEME_ID',
+	id,
+}) // fix any
