3b4fce75fe3235b11d59e39fb00a01920f038bdc
Svyatoslav Nilikovsky <grippiby@gmail.com>
2023-08-03 10:43:41 +0300

    fix to number

--- src/s2-homeworks/hw12/HW12.tsx
+++ src/s2-homeworks/hw12/HW12.tsx
@@ -3,7 +3,6 @@ import s from './HW12.module.css'
 import s2 from '../../s1-main/App.module.css'
 import SuperSelect from '../hw07/common/c5-SuperSelect/SuperSelect'
 import { useDispatch, useSelector } from 'react-redux'
-
 import { changeThemeId } from './bll/themeReducer'
 import { AppStoreType } from '../hw10/bll/store'
 /*
--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -16,10 +16,12 @@ export const themeReducer = (
 	switch (action.type) {
 		// дописать
 		case 'SET_THEME_ID':
-			return {
-				...state,
-				themeId: action.id,
-			}
+			console.log({ ...state, themeId: +action.id })
+			return { ...state, themeId: +action.id }
+		/*	let newState = state
+			newState = { themeId: +action.id }
+			console.log(newState)
+			return newState*/
 		default:
 			return state
 	}
